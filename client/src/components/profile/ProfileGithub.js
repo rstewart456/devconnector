ae6e2e6bee600e59aca948add22c075426fb06a4
rstewart456 <rstewart456@gmail.com>
2020-03-27 16:42:38 -0400

    Adding Commits and ready to deploy

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -16,7 +16,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
                 <div key={repo._id} className="repo bg-white p-1 my-1">
                     <div>
                         <h4>
-                            <a href={repo.html_url} target='_blank' rel='noopener noreferre' >
+                            <a href={repo.html_url} target='_blank' rel='noopener noreferrer' >
                                 {repo.name}
                                 </a>
                         </h4>
